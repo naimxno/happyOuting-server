275b0bd0c2ecd1f7c531960524605e0cbc6562a0
naimxno <naimxno@gmail.com>
2022-05-01 00:32:49 +0600

    mongoDB add

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.lxdts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db('happyOuting').collection('Items');
+    app.get('/items', async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    })
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('get')
 })
 app.listen(port, () => {
   console.log("port => ", port)
