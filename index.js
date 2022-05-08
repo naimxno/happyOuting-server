a3ffff2690d211cb7f4569a6ccfd3940e8c01a9f
naimxno <naimxno@gmail.com>
2022-05-08 10:11:11 +0600

    get and delete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,12 +17,35 @@ async function run() {
   try {
     await client.connect();
     const itemCollection = client.db('happyOuting').collection('Items');
+
     app.get('/items', async (req, res) => {
       const query = {};
       const cursor = itemCollection.find(query);
       const items = await cursor.toArray();
       res.send(items);
+    });
+
+
+    app.get('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemCollection.findOne(query);
+      res.send(item)
+    });
+
+    app.post('/items', async (req, res) => {
+      const newItem = req.body;
+      const result = await itemCollection.insertOne(newItem)
+      res.send(result)
+    });
+
+    app.delete('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemCollection.deleteOne(query);
+      res.send(result)
     })
+
   }
   finally {
 
